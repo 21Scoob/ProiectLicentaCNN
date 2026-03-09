b4763e9b1319fff81274fc01df4f2e2bd96ad957
21Scoob <vgaleriu56@gmail.com>
2026-03-09 19:32:47 +0200

    Script for managing train, validation and test folders for the models

--- CNNtrain.py
+++ CNNtrain.py
@@ -2,41 +2,46 @@ import diffusers
 from torchvision.models import resnet50, ResNet50_Weights
 import torch
 import torchvision
-from torchvision import transforms, models
+from torchvision import models, datasets
+import torchvision.transforms.v2 as transforms_v2
 
 #Data Augmentation for training, validation and test proccess
 
 image_transforms = {
-    "train": transforms.Compose([
-        transforms.RandomResizedCrop(size=256, scale=(0.8,1)),
-        transforms.RandomRotation(degrees=15),
-        transforms.RandomHorizontalFlip(),
-        transforms.CenterCrop(size=224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485,0.456,0.406],
+    "train": transforms_v2.Compose([
+        transforms_v2.RandomResizedCrop(size=256, scale=(0.8,1)),
+        transforms_v2.RandomRotation(degrees=15),
+        transforms_v2.RandomHorizontalFlip(),
+        transforms_v2.CenterCrop(size=224),
+        transforms_v2.ToDtype(torch.float32, scale=True),
+        transforms_v2.Normalize(mean=[0.485,0.456,0.406],
                              std=[0.229,0.224,0.225])
     ]),
         
-    "valid": transforms.Compose([
-        transforms.Resize(size=256),
-        transforms.CenterCrop(size=224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485,0.456,0.406],
+    "valid": transforms_v2.Compose([
+        transforms_v2.Resize(size=256),
+        transforms_v2.CenterCrop(size=224),
+        transforms_v2.ToDtype(torch.float32, scale=True),
+        transforms_v2.Normalize(mean=[0.485,0.456,0.406],
                              std=[0.229,0.224,0.225])
     ]),
     
-    "test": transforms.Compose([
-        transforms.Resize(size=256),
-        transforms.CenterCrop(size=224),
-        transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485,0.456,0.406],
+    "test": transforms_v2.Compose([
+        transforms_v2.Resize(size=256),
+        transforms_v2.CenterCrop(size=224),
+        transforms_v2.ToDtype(torch.float32, scale=True),
+        transforms_v2.Normalize(mean=[0.485,0.456,0.406],
                              std=[0.229,0.224,0.225])
     ])
 }   
+# Loading up the data and start augmenting it
+
+image_datasets = {
+    "train": datasets.ImageFolder('')
+}
 
 #Setting up the model
 
 device = torch.accelerator.current_accelerator().type if torch.accelerator.is_available() else "cpu"
-weights = models.ResNet50_Weights
+weights = models.ResNet50_Weights.IMAGENET1K_V1
 model = models.resnet50(weights=weights).to(device)
-
---
+++ dataclassify.py
@@ -0,0 +1,46 @@
+import os
+import shutil
+import random
+
+folder = r"/Volumes/SSD1/Real"
+
+final_folder = r"/Volumes/SSD1/Dataset_Final"
+
+subfolders = [f.path for f in os.scandir(folder) if f.is_dir()]
+
+for person in subfolders:
+    
+    person_name = os.path.basename(person)
+    
+    files = [f for f in os.listdir(person) if not f.startswith('.')]
+    
+    total_files = len(files)
+    
+    random.shuffle(files)
+    
+    train = int(total_files * 0.8)
+    val = train + int(total_files* 0.1)
+    
+    train_files = files[:train]
+    valid_files = files[train:val]
+    test_files = files[val:]
+    
+    splits = {
+        "train": train_files,
+        "valid": valid_files,
+        "test": test_files        
+}
+    
+    for split_name, file_list in splits.items():
+        
+        dataset_dir = os.path.join(final_folder, split_name, "real", person_name)
+        
+        os.makedirs(dataset_dir, exist_ok=True)
+    
+        for file_name in file_list:
+            
+            src_file = os.path.join(person, file_name)
+            dataset_file = os.path.join(dataset_dir, file_name)
+            
+            shutil.copy2(src_file, dataset_file)
+
